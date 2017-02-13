887a96c1c1111d03bf56963feeeb0507fbf08bd7
Rodrigo Ruas <rodrigoruas2@gmail.com>
2017-02-13 16:02:04 -0200

    boa

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'game', to: 'words_game#game'
+
+  get 'score', to: 'words_game#score'
+
+  root to: 'words_game#game'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
